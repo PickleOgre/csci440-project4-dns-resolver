35192d871c859cb68c3cf4039f21fd71c05ac83c
Josiah Lawrence <skelkor2@gmail.com>
2026-04-17 12:59:18 -0700

    Create resolve_thr function

--- DNS-handout/lookup.c
+++ DNS-handout/lookup.c
@@ -7,13 +7,13 @@
  * Description:
  * 	This file contains the reference non-threaded
  *      solution to this assignment.
- *  
+ *
  */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "util.h"
 
@@ -22,61 +22,62 @@
 #define SBUFSIZE 1025
 #define INPUTFS "%1024s"
 
-int main(int argc, char* argv[]){
+int
+main(int argc, char* argv[])
+{
 
-    /* Local Vars */
-    FILE* inputfp = NULL;
-    FILE* outputfp = NULL;
-    char hostname[SBUFSIZE];
-    char errorstr[SBUFSIZE];
-    char firstipstr[INET6_ADDRSTRLEN];
-    int i;
-    
-    /* Check Arguments */
-    if(argc < MINARGS){
-	fprintf(stderr, "Not enough arguments: %d\n", (argc - 1));
-	fprintf(stderr, "Usage:\n %s %s\n", argv[0], USAGE);
-	return EXIT_FAILURE;
+  /* Local Vars */
+  FILE* inputfp = NULL;
+  FILE* outputfp = NULL;
+  char hostname[SBUFSIZE];
+  char errorstr[SBUFSIZE];
+  char firstipstr[INET6_ADDRSTRLEN];
+  int i;
+
+  /* Check Arguments */
+  if (argc < MINARGS) {
+    fprintf(stderr, "Not enough arguments: %d\n", (argc - 1));
+    fprintf(stderr, "Usage:\n %s %s\n", argv[0], USAGE);
+    return EXIT_FAILURE;
+  }
+
+  /* Open Output File */
+  outputfp = fopen(argv[(argc - 1)], "w");
+  if (!outputfp) {
+    perror("Error Opening Output File");
+    return EXIT_FAILURE;
+  }
+
+  /* Loop Through Input Files */
+  for (i = 1; i < (argc - 1); i++) {
+
+    /* Open Input File */
+    inputfp = fopen(argv[i], "r");
+    if (!inputfp) {
+      sprintf(errorstr, "Error Opening Input File: %s", argv[i]);
+      perror(errorstr);
+      break;
     }
 
-    /* Open Output File */
-    outputfp = fopen(argv[(argc-1)], "w");
-    if(!outputfp){
-	perror("Error Opening Output File");
-	return EXIT_FAILURE;
+    /* Read File and Process*/
+    while (fscanf(inputfp, INPUTFS, hostname) > 0) {
+
+      /* Lookup hostname and get IP string */
+      if (dnslookup(hostname, firstipstr, sizeof(firstipstr)) == UTIL_FAILURE) {
+        fprintf(stderr, "dnslookup error: %s\n", hostname);
+        strncpy(firstipstr, "", sizeof(firstipstr));
+      }
+
+      /* Write to Output File */
+      fprintf(outputfp, "%s,%s\n", hostname, firstipstr);
     }
 
-    /* Loop Through Input Files */
-    for(i=1; i<(argc-1); i++){
-	
-	/* Open Input File */
-	inputfp = fopen(argv[i], "r");
-	if(!inputfp){
-	    sprintf(errorstr, "Error Opening Input File: %s", argv[i]);
-	    perror(errorstr);
-	    break;
-	}	
+    /* Close Input File */
+    fclose(inputfp);
+  }
 
-	/* Read File and Process*/
-	while(fscanf(inputfp, INPUTFS, hostname) > 0){
-	
-	    /* Lookup hostname and get IP string */
-	    if(dnslookup(hostname, firstipstr, sizeof(firstipstr))
-	       == UTIL_FAILURE){
-		fprintf(stderr, "dnslookup error: %s\n", hostname);
-		strncpy(firstipstr, "", sizeof(firstipstr));
-	    }
-	
-	    /* Write to Output File */
-	    fprintf(outputfp, "%s,%s\n", hostname, firstipstr);
-	}
+  /* Close Output File */
+  fclose(outputfp);
 
-	/* Close Input File */
-	fclose(inputfp);
-    }
-
-    /* Close Output File */
-    fclose(outputfp);
-
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
